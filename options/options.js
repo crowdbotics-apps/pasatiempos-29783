1fe23279c5f604abedaa4bb0e2d2670698e7b77e
Crowdbotics <team@crowdbotics.com>
2021-08-13 14:52:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "pasatiempos_29783Identifier",
+  url: "https://pasatiempos_29783Identifier.botics.co",
+  api: "https://pasatiempos_29783Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
